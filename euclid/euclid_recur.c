f80e9946c6ac8c598065e6be356f8e56ea5763ec
ruslanpodlesny <ruslan.podlesny@me.com>
2025-07-08 04:45:27 +0300

    Euclidean algorithm via recursion

---
+++ euclid/euclid_recur.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <assert.h>
+
+int abs(int a) {
+	return (a < 0) ? -a : a;
+}
+
+void read_values(int* pa, int* pb) {
+	printf("enter a and b: ");
+	int scanf_res = scanf_s("%d%d", pa, pb);	
+	if (scanf_res != 2) {
+		printf("invalid input!\n");
+		abort();
+	}
+	else if (*pb == 0) {
+		printf("division by zero!\n");
+		abort();
+	}	
+		
+}
+
+int gcd(int a, int b) {
+	
+	int r = a % b;	
+
+	if (r == 0) {
+		return b;
+	}
+	else {
+		if (r < 0) r += abs(b); // check logic behind this
+		gcd(b, r);
+	}
+		
+}
+
+
+int main(void) {
+
+	int a, b;
+
+	read_values(&a, &b);
+	printf("gcd = %d\n", gcd(a, b));
+	
+	return 0;
+}
